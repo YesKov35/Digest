19b216b8382d5c7eccf95509eb0cc4a729d9ab5b
YesNik <wr35nik@gmail.com>
2022-03-20 13:23:05 +0200

    add help, contact form / update app icon

--- app/src/main/java/com/raisehumaneness/digest/MainActivity.kt
+++ app/src/main/java/com/raisehumaneness/digest/MainActivity.kt
@@ -1,12 +1,17 @@
  package com.raisehumaneness.digest
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.raisehumaneness.digest.data.models.PhoneBookModel
 import com.raisehumaneness.digest.data.repository.CountryRepository
 import com.raisehumaneness.digest.databinding.ActivityMainBinding
+
 
  class MainActivity : AppCompatActivity() {
 
@@ -17,6 +22,8 @@ import com.raisehumaneness.digest.databinding.ActivityMainBinding
          super.onCreate(savedInstanceState)
          binding = ActivityMainBinding.inflate(layoutInflater)
          setContentView(binding.root)
+
+         initContacts()
 
          val navHostFragment =
              supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
@@ -38,5 +45,66 @@ import com.raisehumaneness.digest.databinding.ActivityMainBinding
              dataResult += PhoneBookModel(countryNames[i], phoneBook[i], i)
 
          return dataResult
+     }
+
+     private fun initContacts(){
+         binding.run {
+             contactClose.setOnClickListener { contactView.visibility = View.GONE }
+             info.setOnClickListener { contactView.visibility = View.VISIBLE }
+
+             contact.contactInstUrl.setOnClickListener { openInst() }
+             contact.contactInst.setOnClickListener { openInst() }
+
+             contact.contactMailUrl.setOnClickListener { openEmail() }
+             contact.contactMail.setOnClickListener { openEmail() }
+
+             contact.contactPatreonUrl.setOnClickListener { openPatreon() }
+             contact.contactPatreon.setOnClickListener { openPatreon() }
+         }
+
+     }
+
+     private fun openInst(){
+         val uri: Uri = Uri.parse("http://instagram.com/_u/raisehumaneness")
+         val likeIng = Intent(Intent.ACTION_VIEW, uri)
+
+         likeIng.setPackage("com.instagram.android")
+
+         try {
+             startActivity(likeIng)
+         } catch (e: ActivityNotFoundException) {
+             startActivity(
+                 Intent(
+                     Intent.ACTION_VIEW,
+                     Uri.parse("http://instagram.com/raisehumaneness")
+                 )
+             )
+         }
+     }
+
+     fun openEmail() {
+         val intent = Intent(Intent.ACTION_SENDTO,
+             Uri.parse("mailto:raisehumaneness@gmail.com"))
+         startActivity(Intent.createChooser(intent, "Email"))
+     }
+
+     private fun openPatreon(){
+
+
+         val uri: Uri = Uri.parse("https://www.patreon.com/raisehumaneness")
+         val likeIng = Intent(Intent.ACTION_VIEW, uri)
+
+         likeIng.setPackage("com.patreon.android")
+
+         try {
+             startActivity(likeIng)
+         } catch (e: ActivityNotFoundException) {
+             startActivity(
+                 Intent(
+                     Intent.ACTION_VIEW,
+                     Uri.parse("https://www.patreon.com/raisehumaneness")
+                 )
+             )
+         }
      }
  }
